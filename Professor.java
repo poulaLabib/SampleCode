dd312d2d04da8de49e1e4e1abd8164157a757f15
poulaLabib <169151071+poulaLabib@users.noreply.github.com>
2026-05-17 16:37:26 +0300

    Update from VR Code City

--- Professor.java
+++ Professor.java
@@ -1,5 +1,10 @@
 public class Professor extends Person {
 
+    private static final int GRADE_THRESHOLD_A = 85;
+    private static final int GRADE_THRESHOLD_B = 70;
+    private static final int MEETING_DURATION_LONG = 120;
+    private static final int HOURS_WEIGHT_IN_WORKLOAD = 10;
+
     public Professor(String name, int age) {
         super(name, age);
     }
@@ -11,9 +16,9 @@ public class Professor extends Person {
     public void evaluateStudent(Student student, int grade) {
         System.out.println("Professor: " + name);
         System.out.println("Student: " + student.name);
-        if (grade > 85) {
+        if (grade > GRADE_THRESHOLD_A) {
             System.out.println("Grade: A");
-        } else if (grade > 70) {
+        } else if (grade > GRADE_THRESHOLD_B) {
             System.out.println("Grade: B");
         } else {
             System.out.println("Grade: C");
@@ -21,7 +26,7 @@ public class Professor extends Person {
     }
 
     public void holdMeeting(int durationMinutes) {
-        if (durationMinutes > 120) {
+        if (durationMinutes > MEETING_DURATION_LONG) {
             System.out.println("Long meeting");
         } else {
             System.out.println("Short meeting");
@@ -29,10 +34,10 @@ public class Professor extends Person {
     }
 
     public void printWorkload(int courses, int students, int hours) {
-        int workload = courses * students + hours * 10;
+        int workload = courses * students + hours * HOURS_WEIGHT_IN_WORKLOAD;
         System.out.println("Workload score: " + workload);
     }
     
     public void NewMethod5() {
-}
+    }
 }
